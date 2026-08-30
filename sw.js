c8c6b82848f12a58c888483f0fdeab7e5e4c77ee
rodrigobxl47-glitch <rodrigo.bxl47@gmail.com>
2026-08-30 17:39:26 -0400

    Remover arquivos duplicados da raiz

--- sw.js
+++
@@ -1,8 +0,0 @@
-
-const CACHE='atr-v1';
-const ASSETS=['/','/static/style.css','/static/app.js','/static/manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>{
-  if(e.request.url.includes('/api/')) return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
